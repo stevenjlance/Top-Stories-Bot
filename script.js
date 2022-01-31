b9d531fc88fccf3264fb10f87262e9267e235b17
StevenLance <44912347+stevenjlance@users.noreply.github.com>
2022-01-31 19:42:19 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("connected")
+
+// Select the output div and button below.
+
+var button = document.querySelector("#topButton")
+var outputDiv = document.querySelector("#storyContainer")
+
+
+// Code out an event listener for the button that logs "Button pressed!" to the console when it is clicked
+
+button.addEventListener("click", function(){
+	
+	
+	
+})
+
+// Go to https://developers.nytimes.com/ and create an account. Then create your first app called testApp to create an API key. Enter it below.
+var YOUR_API_KEY_HERE = "AAAmkyE5A5jA1DdPTVUIg9wmubgatxFN";
+
+function sendApiRequest(){
+	var response = fetch(``)
+	
+}
